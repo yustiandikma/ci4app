be768196ad690e595c695d5b3439be2392d9c453
yustiandikma <tianokondank@rocketmail.com>
2021-04-21 20:33:09 +0700

    finish lanjut lagi

--- app/Views/komik/detail.php
+++ app/Views/komik/detail.php
@@ -1,30 +1,32 @@
 <?= $this->extend('layout/template'); ?>
 
 <?= $this->section('content'); ?>
-<div class="con">
-    <div class="row">
-        <div class="col">
-            <h2 class="mt-2">Detail Komik</h2>
-            <div class="card mb-3" style="max-width: 540px;">
-                <div class="row no-gutters">
-                    <div class="col-md-4">
-                        <img src="/img/<?= $komik['sampul']; ?>" class="card-img" alt="...">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title"><?= $komik['judul']; ?></h5>
-                            <p class="card-text"><b>Penulis :</b> <?= $komik['penulis']; ?></p>
-                            <p class="card-text"><small class="text-muted"><b>Penerbit : </b><?= $komik['penerbit']; ?></small></p>
+<div class="container">
+    <div class="con">
+        <div class="row">
+            <div class="col">
+                <h2 class="mt-2">Detail Komik</h2>
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <img src="/img/<?= $komik['sampul']; ?>" class="card-img" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title"><?= $komik['judul']; ?></h5>
+                                <p class="card-text"><b>Penulis :</b> <?= $komik['penulis']; ?></p>
+                                <p class="card-text"><small class="text-muted"><b>Penerbit : </b><?= $komik['penerbit']; ?></small></p>
 
-                            <a href="/komik/edit/<?= $komik['slug']; ?>" class="btn btn-warning">Edit</a>
+                                <a href="/komik/edit/<?= $komik['slug']; ?>" class="btn btn-warning">Edit</a>
 
-                            <form action="/komik/<?= $komik['id']; ?>" method="post" class="d-inline">
-                                <input type="hidden" name="_method" value="DELETE">
-                                <?= csrf_field(); ?>
-                                <button type="submit" class="btn btn-danger" onclick="return confirm('Apakah Anda Yakin Akan Menghapus Data?');">Delete</button>
-                            </form>
-                            <br><br>
-                            <a href="/komik">Kembali Ke Daftar Komik</a>
+                                <form action="/komik/<?= $komik['id']; ?>" method="post" class="d-inline">
+                                    <input type="hidden" name="_method" value="DELETE">
+                                    <?= csrf_field(); ?>
+                                    <button type="submit" class="btn btn-danger" onclick="return confirm('Apakah Anda Yakin Akan Menghapus Data?');">Delete</button>
+                                </form>
+                                <br><br>
+                                <a href="/komik">Kembali Ke Daftar Komik</a>
+                            </div>
                         </div>
                     </div>
                 </div>
