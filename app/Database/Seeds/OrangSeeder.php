2e7b618342a75f6e1ae684174a5b825845926301
yustiandikma <tianokondank@rocketmail.com>
2021-04-27 14:39:54 +0700

    13.seeder & faker

---
+++ app/Database/Seeds/OrangSeeder.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\I18n\Time;
+
+class OrangSeeder extends \CodeIgniter\Database\Seeder
+{
+    public function run()
+    {
+        // $data = [
+        //     [
+        //         'nama'          => 'Yustian Dikma',
+        //         'alamat'        => 'Banyubiru',
+        //         'created_at'    => Time::now(),
+        //         'updated_at'    => Time::now()
+        //     ],
+        //     [
+        //         'nama'          => 'Helisma Putri',
+        //         'alamat'        => 'Bandung',
+        //         'created_at'    => Time::now(),
+        //         'updated_at'    => Time::now()
+        //     ],
+        //     [
+        //         'nama'          => 'Indah Cahya',
+        //         'alamat'        => 'Padang',
+        //         'created_at'    => Time::now(),
+        //         'updated_at'    => Time::now()
+        //     ]
+        // ];
+
+        $faker = \Faker\Factory::create('id_ID');
+        for ($i = 0; $i < 100; $i++) {
+            # code...
+            $data = [
+                'nama'          => $faker->name,
+                'alamat'        => $faker->address,
+                'created_at'    => Time::createFromTimestamp($faker->unixTime()),
+                'updated_at'    => Time::now()
+            ];
+
+            // Using Query Builder
+            $this->db->table('orang')->insert($data);
+        }
+
+
+        // Simple Queries
+        // $this->db->query(
+        //     "INSERT INTO orang (nama, alamat, created_at, updated_at) 
+        //         VALUES(:nama:, :alamat: , :created_at:, :updated_at:)",
+        //     $data
+        // );
+
+        // Using Query Builder For Batch Data
+        // $this->db->table('orang')->insertBatch($data);
+    }
+}
